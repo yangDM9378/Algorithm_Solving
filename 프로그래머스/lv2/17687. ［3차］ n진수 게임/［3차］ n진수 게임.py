74d16b62ca56b2f729c1ba5926a9612dc5cc7976
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-07-09 12:25:38 +0900

    [level 2] Title [3차] n진수 게임 코드 수정

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,23 +1,24 @@
-# n진수로 변환하는 함수
-def convert(number, n):
-    if number == 0:
+def n_wlstn(n,i):
+    li="0123456789ABCDEF"
+    if i == 0:
         return '0'
-    NUMBERS = "0123456789ABCDEF"
-    res = ""
-    while number > 0:
-        number, mod = divmod(number, n)
-        res += NUMBERS[mod]
-    return res[::-1]
-
+    n_wls=''
+    while i > 0:
+        i,mod = divmod(i,n)
+        n_wls += li[mod]
+    return n_wls[::-1]
+    
 def solution(n, t, m, p):
     answer = ''
-    game = ''
-    cur = p - 1
-    for num in range(t * m):
-        game += convert(num, n)
-    while 1:
+    result = ''
+    sum_str=p-1
+
+    for i in range(t*m):
+        result += n_wlstn(n,i)
+    while True:
         if len(answer) == t:
             break
-        answer += game[cur]
-        cur += m
+        answer += result[sum_str]
+        sum_str += m
+
     return answer
