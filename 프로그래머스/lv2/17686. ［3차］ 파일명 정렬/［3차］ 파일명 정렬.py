f300f5128aedf8bcc47089acc6098f775be47d6e
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-07-19 09:43:58 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.40 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,7 +1,6 @@
 def solution(files):
     answer = []
     files_apart = []
-
     for file in files:
         file_apart = []
         file_apart.append(file)
@@ -10,18 +9,19 @@ def solution(files):
                 num_start = i
                 break
         file_apart.append(file[:num_start].upper())
-        
-        # Fix the numeric part extraction
-        num_end = num_start + 1
-        while num_end < len(file) and file[num_end].isdigit():
-            num_end += 1
+        num_end = num_start+1
+        while True:
+            if num_end == len(file):
+                break
+            if not file[num_end].isdigit():
+                break
+            num_end+=1
+                
         file_apart.append(int(file[num_start:num_end]))
-
         files_apart.append(file_apart)
 
-    files_sort = sorted(files_apart, key=lambda x: (x[1], x[2]))
-    
+
+    files_sort = sorted(files_apart, key=lambda x:(x[1], x[2]))
     for file_sort in files_sort:
         answer.append(file_sort[0])
-
     return answer
