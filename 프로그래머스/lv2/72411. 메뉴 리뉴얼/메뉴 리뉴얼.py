dd72a92858f0eb073028da943dd31f3f56dac1d5
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-08-07 13:26:41 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.41 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,26 @@
+from itertools import combinations
+def solution(orders, course):
+    result = []
+    for i in course:
+        course_orders = []
+        order_dic={}
+        for order in orders:
+            order=list(order)
+            order.sort()
+            combination=list(combinations(order,i))
+            for com in combination:
+                course_orders.append(com)
+        for course_order in course_orders:
+            order_dic[course_order] = order_dic.get(course_order,0)
+            order_dic[course_order]+=1
+        order_dic = order_dic.items()
+        order_dic = sorted(order_dic, key=lambda x:-x[1])
+        
+        if len(order_dic)==0:continue
+        max_order_cnt = order_dic[0][1]
+        if max_order_cnt >= 2:
+            for new_course in order_dic:
+                if new_course[1]==max_order_cnt:
+                    result.append(''.join(new_course[0]))
+    result.sort()
+    return result
