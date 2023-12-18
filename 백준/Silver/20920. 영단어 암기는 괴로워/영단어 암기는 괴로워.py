151d1e768be29c3de86c524e9ef9915758d31b12
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2023-12-18 23:38:48 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 480 ms, Memory: 139212 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,18 +1,10 @@
-import sys
-
-n, m = map(int, sys.stdin.readline().split())
+N,M = map(int, input().split())
 word_set = {}
 
-for _ in range(n):
-    word = sys.stdin.readline().strip()
-
-    if len(word) >= m:
-        if word in word_set:
-            word_set[word] += 1
-        else:
-            word_set[word] = 1
-
+for _ in range(N):
+    word = input()
+    if len(word) >= M:
+        word_set[word] = word_set.get(word,1)+1
 word_dict = sorted(word_set.items(),key=lambda x:(-x[1],-len(x[0]),x[0]))
-
 for word in word_dict:
     print(word[0])
