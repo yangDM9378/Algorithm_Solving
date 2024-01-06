d43b4554e8c9c32c83f5c1bc386e71b613a60de8
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2024-01-06 14:56:22 +0900

    [Silver III] Title: 타노스, Time: 112 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,24 +1,13 @@
-def remove_num(num, cnt, check_num):
-    new_word = ''
-    for i in range(len(num)):
-        if num[i] == check_num:
-            cnt -= 1
-            if cnt == 0:
-                tmp = i
-                break
-        else:
-            new_word += num[i]
-    new_word += num[tmp + 1:]
-    return new_word[::-1]
-
 num = input()
-cnt_0 = 0
-cnt_1 = 0
+cnt_0 = num.count('0')/2
+cnt_1 = num.count('1')/2
+new_num=''
 for i in range(len(num)):
-    if num[i]=='0':
-        cnt_0+=1
-    if num[i]=='1':
-        cnt_1+=1
-num = remove_num(num, cnt_1//2, '1')
-num = remove_num(num, cnt_0//2, '0')
-print(num)
+    if num[i]=='1' and cnt_1 == 0:
+        new_num+='1'
+    elif num[i]=='1' and cnt_1 !=0:
+        cnt_1-=1
+    if num[i]=='0' and cnt_0 != 0:
+        cnt_0-=1
+        new_num+='0'
+print(new_num)
