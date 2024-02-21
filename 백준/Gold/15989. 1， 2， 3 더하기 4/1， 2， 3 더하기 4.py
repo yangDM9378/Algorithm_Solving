37cacf4edb61b93d54d1dcfe331f8a90c99ca9d4
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2024-02-21 21:34:29 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 156 ms, Memory: 110720 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -0,0 +1,11 @@
+T=int(input())
+for _ in range(T):
+    num=int(input())
+    dp=[1]*(num+1)
+    for i in range(2,num+1):
+        dp[i] += dp[i-2]
+
+    for i in range(3,num+1):
+        dp[i]+= dp[i-3]
+    print(dp[num])
+
