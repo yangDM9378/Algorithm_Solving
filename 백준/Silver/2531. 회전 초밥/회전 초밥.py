460971401fc40cf277531460676bc423519f1ef0
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2024-03-01 12:59:18 +0900

    [Silver I] Title: 회전 초밥, Time: 152 ms, Memory: 112396 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,15 +1,34 @@
-N,d,k,c = map(int, input().split())
-plate=[]
+N, d, k, c = map(int, input().split())
+plate = []
 
 for _ in range(N):
     plate.append(int(input()))
-for i in range(k-1):
-    plate.append(plate[i])
+
+check = [0] * (d + 1)
+distinct_count = 0
 max_cnt = 0
-for i in range(N):
-    check = [0] * (d + 1)
-    for j in range(k):
-        check[plate[i+j]]=1
-    check[c]=1
-    max_cnt=max(sum(check),max_cnt)
+
+for i in range(k):
+    if check[plate[i]] == 0:
+        distinct_count += 1
+    check[plate[i]] += 1
+
+if check[c] == 0:
+    distinct_count += 1
+check[c] += 1
+max_cnt = distinct_count
+
+for i in range(1, N):
+    if check[plate[i - 1]] == 1:
+        distinct_count -= 1
+    check[plate[i - 1]] -= 1
+
+    if check[plate[(i + k - 1) % N]] == 0:
+        distinct_count += 1
+    check[plate[(i + k - 1) % N]] += 1
+
+    if check[c] == 0:
+        distinct_count += 1
+    max_cnt = max(max_cnt, distinct_count)
+
 print(max_cnt)
