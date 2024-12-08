73b0e90819b4fb9c284c4a1fbc42ac54e09f7b43
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2024-12-08 11:41:12 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.39 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -1,17 +1,20 @@
 function solution(n) {
-    let answer = Array.from({length: n }, () => Array(n).fill(0));
-    const direct = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    let x = 0, y = 0, dir =0; 
+    let answer = Array.from({length: n}, () => Array(n).fill(0));
+    const direct = [[0, 1],[1, 0],[0, -1], [-1, 0]]
+    let x = 0, y = 0, dir = 0
+    
     for (i=1; i<n**2+1; i++) {
         answer[x][y] = i
-        const nx = x + direct[dir][0]
-        const ny = y + direct[dir][1]
-        
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n || answer[nx][ny] !== 0) {
+        const nx = x+direct[dir][0]
+        const ny = y+direct[dir][1]
+        if (nx < 0 || nx > n - 1 || ny < 0 || ny > n - 1 || answer[nx][ny] != 0) {
             dir = (dir + 1) % 4
+    
         }
-        x = x + direct[dir][0];
-        y = y + direct[dir][1];
+        x = x + direct[dir][0]
+        y = y + direct[dir][1]
     }
+
+    
     return answer;
 }
