b66632d335b2b2d196102bdf3055e44e12aec8e5
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2025-02-09 11:29:10 +0900

    [level 2] Title: 숫자의 표현, Time: 1.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,18 +1,14 @@
 def solution(n):
-    ssum = 0
-    cnt = 0
-    s = 1
-    for i in range(1,n+1):
-        ssum += i
-        if ssum == n:
-            cnt+=1
-        if ssum >= n:
-            for j in range(s,i+1):
-                ssum -= j
-                if ssum == n:
-                    cnt+=1
-                if ssum < n:
-                    s=j+1
-                    break
-
-    return cnt
+    total_count = 0
+    current_sum = 0
+    start = 1
+    for end in range(1, n + 1):
+        current_sum += end
+        while current_sum > n:
+            current_sum -= start
+            start += 1
+        
+        if current_sum == n:
+            total_count += 1
+    
+    return total_count
