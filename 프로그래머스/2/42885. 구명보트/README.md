d5933f8a5bc320bc40c4653cb33b0945a6bb61d0
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2025-02-22 11:21:16 +0900

    [level 2] Title: 구명보트, Time: 34.26 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 38.4 MB, 시간: 29.17 ms
+메모리: 37.8 MB, 시간: 34.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 22일 11:17:53
+2025년 02월 22일 11:21:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -2,14 +2,19 @@ function solution(people, limit) {
     var answer = 0;
     let start = 0;
     let end = people.length - 1;
-    people = people.sort((a, b) => b - a);
+    
+    // 오름차순으로 정렬
+    people.sort((a, b) => a - b);
+    
     while (start <= end) {
         let weight = people[start] + people[end];
         if (weight <= limit) {
-            end -= 1;
-        } 
-        start += 1;
-        answer += 1;
+            // 두 사람이 함께 탈 수 있으면 둘 다 태운다
+            start += 1;
+        }
+        // 가장 무거운 사람은 항상 태운다
+        end -= 1;
+        answer += 1;  // 구명보트 한 번 사용
     }
     
     return answer;
