a08439dcb9ab22da248324e3f90cbf61f05d65ce
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2025-03-04 14:46:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -8,14 +8,10 @@ def solution(n, words):
     while q:
         cnt+=1
         word = q.popleft()
-        if word[0] != pre_word[-1]:
+        if (word[0] != pre_word[-1]) or (word in pre_li):
             num = n if cnt % n == 0 else cnt % n
             table = cnt // n if cnt % n == 0  else cnt // n + 1
             return [num , table] 
-        if word in pre_li:
-            num = n if cnt % n == 0 else cnt % n
-            table = cnt // n if cnt % n == 0  else cnt // n + 1
-            return [num , table]
         pre_word = word
         pre_li.append(word)
     return [0,0]
