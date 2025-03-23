056b5a1370d9e1837b868da7b3404695dc8d4ad1
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2025-03-23 13:25:52 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,10 +1,11 @@
-from collections import defaultdict 
-        
 def solution(clothes):
-    dic = defaultdict(list)
-    for clothe in clothes:
-        dic[clothe[1]].append(clothe[0])
+    dic = {}
+    for name, catagory in clothes:
+        if catagory not in dic:
+            dic[catagory] = 1
+        else:
+            dic[catagory] += 1
     cnt = 1
     for key, value in dic.items():
-        cnt *= len(value)+1
+        cnt *= value+1
     return cnt-1
