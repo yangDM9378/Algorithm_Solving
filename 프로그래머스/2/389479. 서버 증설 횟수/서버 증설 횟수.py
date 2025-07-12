2f553ce8ef80e7b53354a7015c62913b8f8eaf69
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2025-07-12 13:24:44 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.09 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -0,0 +1,26 @@
+from collections import deque
+import math
+def solution(players, m, k):
+    answer = 0
+    server_num = 1
+    server_list = deque()
+    for i in range(len(players)):
+        if len(server_list) != 0:
+            end_time, end_server_num = server_list.popleft()
+            if end_time < i:
+                server_num -= end_server_num
+            else:
+                server_list.appendleft([end_time, end_server_num])
+        player = players[i]
+        print(f'{i}, player는 {player}명, 현재 서버 갯수 {server_num}')
+        if player < (server_num)*m:continue
+        need_server_num = math.ceil((player+1)/m)
+        add_server_num = need_server_num - server_num
+        server_num += add_server_num
+        print(f'server, {add_server_num} 대 증설')
+        server_end = [i+k-1, add_server_num]
+        server_list.append(server_end)
+        answer += add_server_num
+        
+
+    return answer
