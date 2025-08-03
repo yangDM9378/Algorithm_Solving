ce611fc93811ea167737132b0d9a6c1b2398e4a4
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2025-08-03 16:11:55 +0900

    [level 2] Title: 땅따먹기, Time: 221.80 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,11 +1,5 @@
 def solution(land):
-    answer = 0
-    dp_map = [[0]*4 for _ in land]
-    for i in range(4):
-        dp_map[0][i] = land[0][i]
-    for depth in range(1, len(land)):
-        for k in range(4):
-            for idx in range(4):
-                if k == idx: continue
-                dp_map[depth][k] = max(dp_map[depth][k],dp_map[depth-1][idx] + land[depth][k])
-    return max(dp_map[-1])
+    for i in range(1, len(land)):
+        for j in range(4):
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+    return max(land[-1])
