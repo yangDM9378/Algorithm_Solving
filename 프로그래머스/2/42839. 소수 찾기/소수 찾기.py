94535b0db5806913636dae2d84110de58074d6c8
Yang Dong Min <109335396+yangDM9378@users.noreply.github.com>
2025-08-09 17:03:29 +0900

    [level 2] Title: 소수 찾기, Time: 659.70 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,23 +1,22 @@
 from itertools import permutations
 import math
 
-def primenumber(x):
-    if x in [0,1]:
-        return False
-    for i in range(2, int(math.sqrt(x)+1)):
-        if x % i == 0:
-            return False
-    return True
-
 def solution(numbers):
     results = set()
     for i in range(1, len(numbers)+1):
-        per_number = permutations(numbers, i)
-        for per_num in per_number:
-            num = int("".join(per_num))
-            results.add(int(num))
-    answer = 0
+        for per_num in permutations(numbers,i):
+            results.add(int("".join(per_num)))
+    max_n = max(results)
+    
+    prime_check = [True] * (max_n+1)
+    prime_check[0], prime_check[1] = False, False
+    for i in range(2, int(math.sqrt(max_n))):
+        if prime_check[i]:
+            for k in range(i*i, max_n + 1, i):
+                prime_check[k] = False
+    
+    answer=0
     for result in results:
-        if primenumber(result):
-            answer += 1
+        if prime_check[result]:
+            answer+=1
     return answer
